330b0f3de1e8a7e8a548b3d60494c1909344b76b
Gnaneshwargoli <gnaneshwarreddy.goli@ricoh-usa.com>
2024-11-25 14:30:39 -0500

    Update GnanPluginTestDev.js

--- GnanPluginTestDev.js
+++ GnanPluginTestDev.js
@@ -32,7 +32,36 @@ export class GnanWebRequestSOAPDev extends LitElement {
       this.who = 'World';
        this.response='';
 
-    }						
+    }	
+
+   // Send SOAP request when the component is first updated 
+    updated(changedProperties) {
+        super.updated(changedProperties);
+        if (!changedProperties.has('response')) { this.makeSoapRequest(); }
+    }
+
+    async makeSoapRequest() {
+        const soapEnvelope = ` <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://www.example.com/webservice"> <soapenv:Header/> <soapenv:Body> <web:YourMethod> <web:Parameter1>Value1</web:Parameter1> <web:Parameter2>Value2</web:Parameter2> </web:YourMethod> </soapenv:Body> </soapenv:Envelope> `;
+        try {
+            const response = await fetch('https://your-soap-service-url', {
+                method: 'POST', headers: {
+                    'Content-Type': 'text/xml; charset=utf-8',
+                    'SOAPAction': 'http://www.example.com/webservice/YourMethod',
+                    // Modify the action based on your SOAP service 
+                },
+                body: soapEnvelope,
+            });
+            if (response.ok) {
+                const xml = await response.text();
+                this.response = xml;
+                // Handle the XML response here 
+            }
+            else { this.response = 'Error: ' + response.statusText; }
+        }
+        catch (error) {
+            this.response = 'Error: ' + error.message;
+        }
+    }
                               
     render() {
       return html`<p>Hello ${this.who} ${this.titlefield}<p/>`;
