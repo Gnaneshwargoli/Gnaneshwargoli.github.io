de74d654364aad8c4be98daf1fa541ab23bfa51e
Gnaneshwargoli <gnaneshwarreddy.goli@ricoh-usa.com>
2024-11-25 20:27:12 -0500

    Update GnanPluginTestDev.js

--- GnanPluginTestDev.js
+++ GnanPluginTestDev.js
@@ -54,7 +54,7 @@ export class GnanWebRequestSOAPDev extends LitElement {
        const createdDate='2017-12-14T21:23:28.843Z';
        const repIDInfo='1234';
         const soapEnvelope = `
-          <soapenv:Envelope xmlns:get=\"http://www.pnc.com/pmt/GetRKIInfoService\" xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\"> 
+          <soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\"> 
               <soapenv:Header> 
               <wsse:Security soapenv:mustUnderstand=\"1\" xmlns:wsse=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd\" xmlns:wsu=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd\">
                   <wsse:UsernameToken wsu:Id=\"UsernameToken-1\">
@@ -73,7 +73,7 @@ export class GnanWebRequestSOAPDev extends LitElement {
             const response = await fetch('https://pmt-sst-qa.pncint.net/pmt-getrkiinfoService', {
                 method: 'POST', headers: {
                     'Content-Type': 'text/xml; charset=utf-8',
-                    'SOAPAction': 'getPNCIHierarchyInfo',
+                    'SOAPAction': 'http://www.pnc.com/pmt/GetRKIInfoService/getPNCIHierarchyInfo',
                     // Modify the action based on your SOAP service 
                 },
                 body: soapEnvelope,
